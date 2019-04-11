d87ce3eaa2c88f5ed0f265b04596eea9033f07a5
gobimcp <gobi.mcp@gmail.com>
2019-04-11 08:36:24 -0400

    sub branch commit

--- src/main/java/array/search/Application.java
+++ src/main/java/array/search/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println("Hi - sub");
 		SpringApplication.run(Application.class, args);
 	}
 }
